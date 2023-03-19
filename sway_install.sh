db818d0ac6634c6c91dfddbf72c2c21e558179f3
Antonio Alonso <alloant@gmail.com>
2023-03-19 17:59:23 +0530

    scripts

--- sway_install.sh
+++ sway_install.sh
@@ -1,2 +1,48 @@
-apk add eudev
+apk add shadow shadow-login
+
+apk add util-linux pciutils usbutils coreutils binutils findutils grep iproute2 curl
+
+apk add bash bash-doc bash-completion
+
+apk add zsh zsh-doc 
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+apk add neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+chsh
+
+apk add elogind polkit-elogind
+rc-update add elogind
+rc-service elogind start
+
+sed -i 's|#HandlePowerKey=poweroff|HandlePowerKey=suspend|' /etc/elogind/logind.conf
+
+apk add udisks2 udisks2-doc
+
+apk add stow git
+
+
+apk add eudev eudev-netifnames
 setup-dved udev
+
+apk add iwd dhcpcd
+rc-update del networking
+rc-update add iwd boot
+rc-update add dhcpcd boot
+
+apk add mesa-dri-gallium libva-intel-driver mesa-vulkan-intel
+
+adduser $USER input
+adduser $USER video
+adduser $USER audio
+adduser $USER wheel
+adduser $USER seat
+
+apk add font-dejavu
+
+apk add sway sway-doc xwayland foot alacritty bemenu swaylock swaylockd swaybg swayidle waybar
+
+
---
+++ zsh/.profile
@@ -0,0 +1,26 @@
+export MOZ_ENABLE_WAYLAND="1"
+
+export XDG_CURRENT_DESKTOP=river
+export XDG_SESSION_TYPE=wayland
+export XDG_DOWNLOAD_DIR="/home/antonio/Downloads"
+export XDG_DATA_DIRS="/home/antonio/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share"
+
+export QT_QPA_PLATFORM=wayland-egl
+export QT_QPA_PLATFORMTHEME=qt5ct
+
+export BEMENU_BACKEND=wayland
+export BEMENU_OPTS="-i -l --tb white --fn 'hack 12'"
+
+export LIBVA_DRIVER_NAME=iHD
+
+export GTK_USE_PORTAL=0
+
+export EDITOR=nvim
+export OPENER=mimeo
+
+export XKB_DEFAULT_LAYOUT='us,us'
+export XKB_DEFAULT_VARIANT=',alt-intl'
+export XKB_DEFAULT_OPTIONS='grp:alt_shift_toggle'
+
+export PRODUCTION='local'
+
